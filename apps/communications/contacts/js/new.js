71e717ce2a1194f0d20fc6677b68b6a2b1151cc0
Evan Xd <itoyxd@gmail.com>
2013-03-26 17:14:33 +0800

    Refactory the code: Extract the two reusable modules(contacts_tag.js and contacts_web_activity.js) from new.js.

--- apps/communications/contacts/js/new.js
+++ apps/communications/contacts/js/new.js
@@ -2,33 +2,12 @@
 
 var NewContacts = (function() {
   var navigation = new navigationStack('view-contact-form');
-  var TAG_OPTIONS;
-  var contactsForm;
-  var selectedTag,
-      customTag,
-      contactTag;
 
-  var checkUrl = function checkUrl() {
-    initForm(function onInitForm() {
-      var extractParams = function extractParams(url) {
-        if (!url) {
-          return -1;
-        }
-        var ret = {};
-        var params = url.split('&');
-        for (var i = 0; i < params.length; i++) {
-          var currentParam = params[i].split('=');
-          ret[currentParam[0]] = currentParam[1];
-        }
-        return ret;
-      };
-      var hasParams = window.location.hash.split('?');
-      var params = hasParams.length > 1 ?
-      extractParams(hasParams[1]) : -1;
-      if (params !== -1 && 'tel' in params) {
-        contactsForm.render(params);
-      }
-    });
+  var showViewContactForm = function showViewContactForm(params) {
+    var contactsForm = contacts.Form;
+    if (params !== -1 && 'tel' in params) {
+      contactsForm.render(params);
+    }
   };
 
   var handleBack = function handleBack() {
@@ -41,157 +20,47 @@ var NewContacts = (function() {
     }
   };
 
-  var initForm = function c_initForm(callback) {
-    contactsForm = contacts.Form;
-    contactsForm.init(TAG_OPTIONS);
-    callback();
-  };
-
-  var updatePhoto = function updatePhoto(photo, dest) {
-    var background = '';
-    if (photo != null) {
-      background = 'url(' + URL.createObjectURL(photo) + ')';
-    }
-    dest.style.backgroundImage = background;
-  };
-
-  // Checks if an object fields are empty, by empty means
-  // field is null and if it's an array it's length is 0
-  var isEmpty = function isEmpty(obj, fields) {
-    if (obj == null || typeof(obj) != 'object' ||
-        !fields || !fields.length) {
-      return true;
-    }
-    var attr;
-    var isArray;
-    for (var i = 0; i < fields.length; i++) {
-      attr = fields[i];
-      if (obj.hasOwnProperty(attr) && obj[attr]) {
-        if (Array.isArray(obj[attr])) {
-          if (obj[attr].length > 0) {
-            return false;
-          }
-        } else {
-          return false;
-        }
-      }
-    }
-    return true;
-  };
-
-  var goToSelectTag = function goToSelectTag(event) {
-    var target = event.currentTarget.children[0];
-    var tagList = target.dataset.taglist;
-    var options = TAG_OPTIONS[tagList];
-    fillTagOptions(options, tagList, target);
-    navigation.go('view-select-tag', 'right-left');
-    window.navigator.mozKeyboard.removeFocus();   
-  };
-
-  var fillTagOptions = function fillTagOptions(options, tagList, update) {
-    var container = document.getElementById('tags-list');
-    container.innerHTML = '';
-    contactTag = update;
-
-    var selectedLink;
-    for (var option in options) {
-      var link = document.createElement('button');
-      link.dataset.index = option;
-      link.textContent = options[option].value;
-
-      link.onclick = function(event) {
-        var index = event.target.dataset.index;
-        selectTag(event.target, tagList);
-        event.preventDefault();
-      };
-
-      if (update.textContent == TAG_OPTIONS[tagList][option].value) {
-        selectedLink = link;
-      }
-
-      var list = document.createElement('li');
-      list.appendChild(link);
-      container.appendChild(list);
-    }
-
-    // Deal with the custom tag, clean or fill
-    customTag.value = '';
-    if (!selectedLink && update.textContent) {
-      customTag.value = update.textContent;
-    }
-
-    selectTag(selectedLink);
-  };
-
   var onCustomTagSelected = function onCustomTagSelected() {
+    var contactsTag = ContactsTag;
+    var selectedTag = contactsTag.getSelectedTag();
+
     if (selectedTag) {
       // Remove any mark if we had selected other option
       selectedTag.removeAttribute('class');
     }
-    selectedTag = null;
-  };
-
-  var selectTag = function selectTag(link, tagList) {
-    if (link == null) {
-      return;
-    }
-
-    //Clean any trace of the custom tag
-    customTag.value = '';
-
-    var index = link.dataset.index;
-
-    if (selectedTag) {
-      selectedTag.removeAttribute('class');
-    }
-
-    link.className = 'icon icon-selected';
-    selectedTag = link;
+    contactsTag.setSelectedTag(null);
   };
 
   var doneTag = function doneTag() {
-    if (selectedTag) {
+    var contactsTag = ContactsTag;
+    var selectedTag = contactsTag.getSelectedTag();
+    var customTag = contactsTag.getCustomTag();
+    var contactTag = contactsTag.getContactTag();
+
+    if (selectedTag instanceof Object) {
       contactTag.textContent = selectedTag.textContent;
     } else if (customTag.value.length > 0) {
       contactTag.textContent = customTag.value;
     }
-    contactTag = null;
+    contactsTag.setContactTag(null);
     handleBack();
   };
 
   var saveContact = function saveContact() {
-    return contacts.Form.saveContact();
+    var contactsForm = contacts.Form;
+    return contactsForm.saveContact();
   };
 
   var newField = function newField(evt) {
-    return contacts.Form.onNewFieldClicked(evt);
+    var contactsForm = contacts.Form;
+    return contactsForm.onNewFieldClicked(evt);
   };
 
   var initContainers = function initContainers() {
-    var _ = navigator.mozL10n.get;
-    customTag = document.getElementById('custom-tag');
-    
-    TAG_OPTIONS = {
-      'phone-type' : [
-        {value: _('mobile')},
-        {value: _('home')},
-        {value: _('work')},
-        {value: _('personal')},
-        {value: _('faxHome')},
-        {value: _('faxOffice')},
-        {value: _('faxOther')},
-        {value: _('another')}
-      ],
-      'email-type' : [
-        {value: _('personal')},
-        {value: _('home')},
-        {value: _('work')}
-      ],
-      'address-type' : [
-        {value: _('home')},
-        {value: _('work')}
-      ]
-    };    
+    var contactsForm = contacts.Form;
+    var contactsTag = ContactsTag;
+    var tagOptions = contactsTag.getTagOptions();
+    contactsForm.init(tagOptions);
   };
 
   var initEventListeners = function initEventListener() {
@@ -213,34 +82,48 @@ var NewContacts = (function() {
     });    
   };
 
-  var init = function init() {
+  var initContacts = function initContacts() {
+    var contactsWebActivity = Contacts;
+    var contactsTag = ContactsTag;
+
+    contactsWebActivity.setNavigation(navigation);
+    contactsTag.loadTagOptions();
     initContainers();
     initEventListeners();
-    window.addEventListener('hashchange', checkUrl);
   };
 
-  var onLocalized = function onLocalized() {
-    init();
-    checkUrl();
-  };
+  var showApp = function showApp() {
+    document.body.classList.remove('hide');
+  }
 
   return {
-    'onLocalized': onLocalized,
-    'goToSelectTag': goToSelectTag,
-    'updatePhoto': updatePhoto,
-    'isEmpty': isEmpty
+    'handleCancel': handleCancel,
+    'initContacts': initContacts,
+    'showViewContactForm': showViewContactForm,
+    'showApp': showApp
   };
 })();
-
-var Contacts = NewContacts;
 
 window.addEventListener('localized', function initContacts(evt) {
   window.removeEventListener('localized', initContacts);
 
-  NewContacts.onLocalized();
+  var showViewContactForm = function showViewContactForm(activity) {
+    var params = activity.source.data.params;
+    var activityHandler = ActivityHandler;
+    
+    try {
+      activityHandler._currentActivity = activity;
+      NewContacts.initContacts();
+      NewContacts.showViewContactForm(params);
+      NewContacts.showApp();
+    } catch(e) {
+      console.error(e);
+    } 
+  };
 
-  if (window.navigator.mozSetMessageHandler && window.self == window.top) {
-    var actHandler = ActivityHandler.handle.bind(ActivityHandler);
-    window.navigator.mozSetMessageHandler('activity', actHandler);
-  }
+  window.navigator.mozSetMessageHandler('activity', showViewContactForm);
+});
+
+document.addEventListener('mozvisibilitychange', function closeContactsActivity() {
+  NewContacts.handleCancel();
 });
