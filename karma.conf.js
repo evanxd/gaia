37cc1864bbd7ad54db45aa6a22664f652d946f7d
Evan Xd <itoyxd@gmail.com>
2014-07-17 12:06:18 +0800

    Use karma-test-agent plugin

--- karma.conf.js
+++ karma.conf.js
@@ -3,11 +3,10 @@
 module.exports = function(config) {
   config.set({
     files: [
-      'setup.js',
       'apps/video/js/video_utils.js',
       'apps/video/test/unit/video_utils_test.js'
     ],
-    frameworks: ['mocha', 'chai-sinon'],
+    frameworks: ['mocha', 'chai-sinon', 'test-agent'],
     client: {
       mocha: {
         ui: 'tdd'
